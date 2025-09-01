ad1b462e7dee652730ef4b1f3535144c2a7a7c37
hybridkid499 <Memz499.manzanares@gmail.com>
2025-08-31 22:46:24 -0300

    actualizacion de nombres entrega 2

--- src/data/products.js
+++ src/data/products.js
@@ -1,10 +1,10 @@
 export const PRODUCTS = [
-  { id: '1', title: 'Remera Naruto Ichiraku',  price: 12000, category: 'remeras', stock: 12, img: '/img/remera-byte.jpg' },
-  { id: '2', title: 'Remera One Piece Going Merry',  price: 13500, category: 'remeras', stock: 10, img: '/img/remera-react.jpg' },
-  { id: '3', title: 'Hoodie Attack on Titan Survey Corps',  price: 32000, category: 'hoodies', stock: 5,  img: '/img/hoodie-fullstack.jpg' },
-  { id: '4', title: 'Hoodie Demon Slayer Nezuko',  price: 34000, category: 'hoodies', stock: 7,  img: '/img/hoodie-byte.jpg' },
-  { id: '5', title: 'Gorra Pokémon Pokéball',  price:  9000, category: 'accesorios', stock: 20, img: '/img/gorra-dev.jpg' },
-  { id: '6', title: 'Medias Dragon Ball Kame',  price:  6000, category: 'accesorios', stock: 25, img: '/img/medias-debug.jpg' },
+  { id: '1', title: 'Remera Naruto Ichiraku',  price: 12000, category: 'remeras', stock: 12, img: '/img/Remera Naruto Ichiraku.jpg' },
+  { id: '2', title: 'Remera One Piece Going Merry',  price: 13500, category: 'remeras', stock: 10, img: '/img/Remera One Piece Going Merry.jpg' },
+  { id: '3', title: 'Hoodie Attack on Titan Survey Corps',  price: 32000, category: 'hoodies', stock: 5,  img: '/img/Hoodie Attack on Titan Survey Corps.jpg' },
+  { id: '4', title: 'Hoodie Demon Slayer Nezuko',  price: 34000, category: 'hoodies', stock: 7,  img: '/img/Hoodie Demon Slayer Nezuko.jpg' },
+  { id: '5', title: 'Gorra Pokémon Pokéball',  price:  9000, category: 'accesorios', stock: 20, img: '/img/Gorra Pokémon Pokéball.jpg' },
+  { id: '6', title: 'Medias Dragon Ball Kame',  price:  6000, category: 'accesorios', stock: 25, img: '/img/Medias Dragon Ball Kame.jpg' },
 ];
 
 export const getProducts = (categoryId) =>
